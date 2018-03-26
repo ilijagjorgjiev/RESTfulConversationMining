a53e463fd0c871440a716e445ca425d8ec3b2ca0
ilijagjogjiev <gjorgi@usi.ch>
2018-03-26 22:33:50 +0200

    added the modifications that we did today, I will continue with the work after Wednesday

--- Code/Sandbox/Parser.js
+++ Code/Sandbox/Parser.js
@@ -14,13 +14,14 @@ var clients = {};
 for(let i = 0; i < links.length; i++){
   let str = links[i];
   let spaces = str.split(' ');
-  console.log(links[i])
-  console.log(spaces)
+  if(str != ''){
   let obj = {date : spaces[0], time : spaces[1], ip : spaces[2], method : spaces[3], location: spaces[4], status : spaces[5]}
   logs[i] = obj;
-  // console.log(links[i])
-   console.log(logs[i])
 }
+  // console.log(links[i])
+   // console.log(logs[i])
+}
+console.log(logs)
 
 //Bug: the logs may contain some undefined entries. Fixed the bug with undefined it was due to an empty line.
 
@@ -34,36 +35,36 @@ for(let i = 0; i < logs.length; i++){
 }
 // console.log(clients)
 //sort by date/time
-// var compare = function(a,b) {
-//   if(a > b) return 1
-//   else if (a === b) return 0
-//   else return -1
-// }
-// for(var ip in clients){
-//   if(ip !== undefined){
-//     for(let i = 0; i < clients[ip].length; i++){
-//       console.log(clients[ip][i])
-//       let build1 = clients[ip][i].date.split('/')
-//       let build2 = clients[ip][i].time.split(':')
-//       let date = new Date();
-//       date.setDate(build1[0])
-//       date.setMonth(build1[1])
-//       date.setFullYear(build1[2])
-//       date.setHours(build2[0])
-//       date.setMinutes(build2[1])
-//       date.setSeconds(build2[2])
-//       clients[ip][i].datetime = date;
-//       //delete clients["154.103.165.66"][i].time;
-//       // console.log(clients["154.103.165.66"][i].date);
-//     }
-//   }
-// }
-// for(var ip in clients){
-//   // console.log(ip)
-//   clients[ip].sort((a,b) => compare(a.datetime,b.datetime));
-// }
+var compare = function(a,b) {
+  if(a > b) return 1
+  else if (a === b) return 0
+  else return -1
+}
+for(var ip in clients){
+  if(ip !== undefined){
+    for(let i = 0; i < clients[ip].length; i++){
+      console.log(clients[ip][i])
+      let build1 = clients[ip][i].date.split('/')
+      let build2 = clients[ip][i].time.split(':')
+      let date = new Date();
+      date.setDate(build1[0])
+      date.setMonth(build1[1])
+      date.setFullYear(build1[2])
+      date.setHours(build2[0])
+      date.setMinutes(build2[1])
+      date.setSeconds(build2[2])
+      clients[ip][i].datetime = date;
+      //delete clients["154.103.165.66"][i].time;
+      // console.log(clients["154.103.165.66"][i].date);
+    }
+  }
+}
+for(var ip in clients){
+  // console.log(ip)
+  clients[ip].sort((a,b) => compare(a.datetime,b.datetime));
+}
 
-// console.log(clients)
+console.log(clients['154.103.165.66'])
 
 var filepath = "data.js"
 var content = "var clients = " + JSON.stringify(clients);
