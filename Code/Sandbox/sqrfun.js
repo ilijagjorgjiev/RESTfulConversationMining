7769bd83f67ea790af1557a651e7949ae86302c2
ilijagjogjiev <gjorgi@usi.ch>
2018-05-23 09:05:07 +0200

    fixed bug with time periods

--- Code/Sandbox/sqrfun.js
+++ Code/Sandbox/sqrfun.js
@@ -258,6 +258,7 @@ var differenceThreshold = function(client){
   let diffThreshold = (min+max)/2;
   var timeP = []
   timeP.push(client[0])
+  console.log(client.length);
   for(let i = 1; i < client.length; i++){
     let date = new Date(client[i-1].datetime)
     let date1 = new Date(client[i].datetime)
@@ -268,6 +269,7 @@ var differenceThreshold = function(client){
     }
     timeP.push(client[i]);
   }
+  if(timeP != []) timePeriods.push(timeP);
   return timePeriods;
 }
 var checkIfIncomingXorExists = function(nodes, key, incomingXorNodes, size, inXorId){
